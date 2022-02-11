acab97651684e74a96239960fa9aeb633e097c5c
Gianna Julio <46408848+g-juli0@users.noreply.github.com>
2022-02-10 20:01:59 -0500

    completed lab 4

--- JavaLabs/Lab 4/src/base/Main.java
+++ JavaLabs/Lab 4/src/base/Main.java
@@ -1,4 +1,4 @@
-package base;
+package src.base;
 
 public class Main {
     /**
@@ -16,8 +16,45 @@ public class Main {
      */
     public static void main(String[] args) {
         // Give me an example of you using switch case.
+        char grade = 'C';
+
+        switch(grade){
+            case 'A':
+                System.out.println("Good job!"); break;
+            case 'B':
+                System.out.println("Almost there!"); break;
+            case 'C':
+                System.out.println("Average."); break;
+            case 'D':
+                System.out.println("Below average."); break;
+            default:
+                System.out.println("Study harder!"); break;
+        }
+
         // Give me an example of you using a for loop
+        for(int i = 1; i <= 5; i++){
+            System.out.print("Line #");
+            System.out.println(i);
+        }
+
         // Give me an example of you using a while loop
+        int sum = 0;
+        
+        while(sum < 20){
+            sum += (int) (Math.random() * 5);
+        }
+
+        System.out.println(sum);
+
+        // function calls
+        System.out.println(exponent(2, 3));
+
+        int[] arr = {2,3,5,2,3,6,7,8,2};
+        System.out.println(count(arr, 2));
+
+        System.out.println(summation(arr));
+
+        System.out.println(pythagorean(3, 4));
     }
     // For the following todos you'll probably want to define them below and then call them from the main function above.
 
@@ -27,17 +64,48 @@ public class Main {
      * and should print out or return 8. Print the output.
      */
 
+    static int exponent(int base, int power){
+        int total = 1;
+        for(int i = 0; i < power; i++){
+            total *= base;
+        }
+
+        return total;
+    }
+
     /**
      * I want you to write a function that will take in a list and see how many times a given number is in the list.
      * For example if the array given is [2,3,5,2,3,6,7,8,2] and the number given is 2 the function should print out
      * or return 3. Print the output.
      */
 
+    static int count(int[] array, int given){
+        int count = 0;
+
+        for(int num : array){
+            if(num == given){
+                count++;
+            }
+        }
+
+        return count;
+     }
+
     /**
      * Create a function named summation that will take in an int[ ] and return an int.
      * I want the function to then go through the given array and sum up digits in the array.
      * Once that’s done print or return the answer. Print the output
      */
+
+    static int summation(int[] array){
+        int sum = 0;
+
+        for(int num : array){
+            sum += num;
+        }
+
+        return sum;
+     }
 
     /**
      * Give me a function that gives the answer to the pythagorean theorem.
@@ -51,13 +119,20 @@ public class Main {
      * Once that’s done print or return the answer. Print the output
      */
 
-    int addition(int a, int b) {
+    static int addition(int a, int b) {
         int answer = a + b;
         return answer;
     }
 
-    int division(int a, int b) {
+    static int division(int a, int b) {
         int answer = a / b;
         return answer;
+    }
+
+    static int pythagorean(int a, int b){
+        int asquared = exponent(a, 2);
+        int bsquared = exponent(b, 2);
+        int csquared = addition(asquared, bsquared);
+        return (int) Math.sqrt(csquared);
     }
 }
