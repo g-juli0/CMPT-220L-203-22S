b38a2333fd9fc24dd71f1b77e85d4ba88bd92a7d
Gianna Julio <46408848+g-juli0@users.noreply.github.com>
2022-02-24 21:46:44 -0500

    lab #6 lists practice completed

--- JavaLabs/Lab 6/src/base/Main.java
+++ JavaLabs/Lab 6/src/base/Main.java
@@ -1,5 +1,10 @@
 package base;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Main {
     /**
      * # Lab Six
@@ -11,9 +16,9 @@ public class Main {
      * Our Goals are:
      * - Creating an Array
      * - Creating an ArrayList
-     * - Creating an LinkedList
-     * - Creating an Queue
-     * - Creating an Stack
+     * - Creating a LinkedList
+     * - Creating a Queue
+     * - Creating a Stack
      * - Create a Plant
      *
      * Please show the ability to:
@@ -22,11 +27,50 @@ public class Main {
      * - Print out the Linear Data Types
      */
     public static void main(String[] args) {
+
         // Create an Array, Populate it, and Print it out
+        int[] nums1 = {0, 1, 2, 3, 4};
+        
+        for(int i : nums1) {
+            System.out.println(i);
+        }
+
         // Create an ArrayList, Populate it, and Print it out
+        ArrayList<Integer> nums2 = new ArrayList<Integer>();
+
+        for(int i = 0; i < 5; i++) {
+            nums2.add(i);
+        }
+        
+        System.out.println(nums2);
+
         // Create an LinkedList, Populate it, and Print it out
+        LinkedList<Integer> nums3 = new LinkedList<Integer>();
+
+        for(int i = 0; i < 5; i++) {
+            nums3.add(i);
+        }
+        
+        System.out.println(nums3);
+
         // Create an Queue, Populate it, and Print it out
+        Queue<Integer> nums4 = new LinkedList<Integer>();
+
+        for(int i = 0; i < 5; i++) {
+            nums4.add(i);
+        }
+
+        System.out.println(nums4);
+
         // Create an Stack, Populate it, and Print it out
+        Stack<Integer> nums5 = new Stack<Integer>();
+
+        for(int i = 0; i < 5; i++) {
+            nums5.push(i);
+        }
+
+        System.out.println(nums4);
+
         // Using the information in the Lab 6 Jupyter Notebook create an instance of plant here.
     }
 }
