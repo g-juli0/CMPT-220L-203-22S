aef83b2437e762a8eabd100041cbc451f9544416
Gianna Julio <46408848+g-juli0@users.noreply.github.com>
2022-03-27 12:17:17 -0400

    added javadoc and made ListItem class more robust

--- JavaLabs/Lab 7/ListItem.java
+++ JavaLabs/Lab 7/ListItem.java
@@ -1,11 +1,62 @@
+/**
+ * @author Gianna Julio
+ * 
+ * ListItem class allows user to chain together multiple
+ * values similat to a LinkedList (i think)
+ */
 public class ListItem {
 
+    // initialize local variables
     int value;
     ListItem next;
 
+    /**
+     * 
+     * @param value integer value being added to the list
+     * @param next the next object in the chain of ListItems
+     */
     public ListItem(int value, ListItem next) {
         this.value = value;
         this.next = next;
+    }
+
+    /**
+     * 
+     * @param val new value to set the ListItem's integer value to
+     */
+    public setValue(int val) {
+        value = val;
+    }
+
+    /**
+     * 
+     * @return value of ListItem
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * 
+     * @param next new ListItem to link to
+     */
+    public setNext(ListItem next) {
+        this.next = next;
+    }
+
+    /**
+     * 
+     * @return next ListItem in chain
+     */
+    public ListItem getNextItem() {
+        return next;
+    }
+
+    /**
+     * toString function - returns String of value
+     */
+    public String toString() {
+        return Integer.toString(value);
     }
 
 }
